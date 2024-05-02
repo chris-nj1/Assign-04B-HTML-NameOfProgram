c62a317819021ecfab045b70b7d7d755a7310eb6
chris-nj1 <159803087+chris-nj1@users.noreply.github.com>
2024-05-02 21:20:29 +0000

    name

---
+++ script.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 Njoku chris All rights reserved
+//
+// Created by: Njoku chris
+// Created on: March 2024
+// This file contains the JS functions for index.html
+
+//The JavaScript helps the user to order food
+
+"use strict";
